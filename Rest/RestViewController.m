b26ddbacd0987bdc34626401305592b545515838
Daniel Rector <rector.danielk@gmail.com>
2015-09-10 01:16:24 -0400

    made that app do work

--- Rest/RestViewController.m
+++ Rest/RestViewController.m
@@ -14,10 +14,30 @@
 
 @implementation RestViewController
 
+- (IBAction)fetchGreeting;
+{
+    NSURL *url = [NSURL URLWithString:@"http://rest-service.guides.spring.io/greeting"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [NSURLConnection sendAsynchronousRequest:request
+                                       queue:[NSOperationQueue mainQueue]
+                           completionHandler:^(NSURLResponse *response,
+                                               NSData *data, NSError *connectionError)
+     {
+         if (data.length > 0 && connectionError == nil)
+         {
+             NSDictionary *greeting = [NSJSONSerialization JSONObjectWithData:data
+                                                                      options:0
+                                                                        error:NULL];
+             self.greetingId.text = [[greeting objectForKey:@"id"] stringValue];
+             self.greetingContent.text = [greeting objectForKey:@"content"];
+         }
+     }];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    [self fetchGreeting];
 }
 
 - (void)didReceiveMemoryWarning
